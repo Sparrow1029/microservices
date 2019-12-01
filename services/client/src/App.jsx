1a417b966ce019c03981663084c4b4a0c03c7d25
Sparrow1029 <sparrow@disroot.org>
2019-12-01 12:45:51 -0700

    Updated navbar to hide options

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Route, Switch } from 'react-router-dom';
 
+import UserStatus from './components/UserStatus';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import Logout from './components/Logout';
@@ -95,7 +96,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar title={this.state.title} />
+        <NavBar
+          title={this.state.title}
+          isAuthenticated={this.state.isAuthenticated}
+        />
         <section className="section">
           <div className="container">
             <div className="columns">
@@ -138,6 +142,11 @@ class App extends Component {
                   <Route exact path='/logout' render={() => (
                     <Logout
                       logoutUser={this.logoutUser}
+                      isAuthenticated={this.state.isAuthenticated}
+                    />
+                  )} />
+                  <Route exact path='/status' render={() => (
+                    <UserStatus
                       isAuthenticated={this.state.isAuthenticated}
                     />
                   )} />
